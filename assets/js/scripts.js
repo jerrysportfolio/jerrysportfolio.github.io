1913f3a37265c77bbc0b200c499523bdbbd75a28
Jerry Hu <work.jerrywu@gmail.com>
2024-11-08 02:30:59 -0800

    Add 'Work' link to navigation and update 'Projects' label; clean up work.html structure

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,13 +13,3 @@ mobileMenuClose.addEventListener('click', function () {
     mobileMenu.classList.add('translate-x-full');
     mobileMenu.classList.remove('translate-x-0');
 });
-
-document.getElementById('send').addEventListener('click', function () {
-    name = document.getElementById('name').value;
-    email = document.getElementById('email').value;
-    subject = document.getElementById('subject').value;
-    message = document.getElementById('message').value;
-
-    // draft email
-    window.location = `mailto:${email}?subject=${subject}&body=${message}`;
-});
