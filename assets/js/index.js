cb50bb384413c0a3dfbec3e520dabdd447a4b248
Jerry Hu <work.jerrywu@gmail.com>
2025-01-21 22:01:50 -0800

    polish cursor

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     if (isMobileDevice()) {
+        document.getElementById('cursor').style.display = 'none';
+        
         document.getElementById('projects').innerHTML = `
         <div class="container grid grid-cols-12 gap-8">
             <div class="col-span-12 md:col-span-6">
@@ -129,5 +131,39 @@ document.addEventListener('DOMContentLoaded', function () {
         </div>
     </div>
 </div>`;
+
+        const cursor = document.getElementById("cursor");
+        const cursorBallBig = document.querySelector(".circle-big");
+        const cursorBallSmall = document.querySelector(".circle-small");
+
+        let posS = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+        let posB = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+        let mouse = { x: posS.x, y: posS.y };
+        const speed = 0.1;
+        let fpms = 60 / 1000;
+
+        window.addEventListener("mousemove", (e) => {
+            mouse.x = e.x;
+            mouse.y = e.y;
+        });
+
+        const xSetBallSmall = gsap.quickSetter(cursorBallSmall, "x", "px");
+        const ySetBallSmall = gsap.quickSetter(cursorBallSmall, "y", "px");
+        const xSetBallBig = gsap.quickSetter(cursorBallBig, "x", "px");
+        const ySetBallBig = gsap.quickSetter(cursorBallBig, "y", "px");
+
+        gsap.ticker.add((time, deltaTime) => {
+            let delta = deltaTime * fpms;
+            let dt = 1.0 - Math.pow(1.0 - speed, delta);
+
+            posS.x += mouse.x - posS.x;
+            posS.y += mouse.y - posS.y;
+            posB.x += (mouse.x - posB.x) * dt;
+            posB.y += (mouse.y - posB.y) * dt;
+            xSetBallSmall(posS.x);
+            ySetBallSmall(posS.y);
+            xSetBallBig(posB.x);
+            ySetBallBig(posB.y);
+        });
     }
 });
