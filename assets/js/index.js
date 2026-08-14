4c275c55680699139e18da5edc7d52907584909f
Jerry Hu <work.jerrywu@gmail.com>
2026-08-14 01:23:32 -0700

    housekeeping stuff

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-    function isMobileDevice() {
-        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  function isMobileDevice() {
+    return (
+      typeof window.orientation !== "undefined" ||
+      navigator.userAgent.indexOf("IEMobile") !== -1
+    );
+  }
 
-    if (isMobileDevice()) {
-        document.getElementById('projects').innerHTML = `
+  if (isMobileDevice()) {
+    document.getElementById("projects").innerHTML = `
         <div class="container grid grid-cols-12 gap-8">
             <div class="col-span-12 md:col-span-6">
+                <!-- Project card -->
+                <a href="https://github.com/AccessRetrieved/RoombaRat" class="block relative mb-16">
+                    <img class="relative -z-10" src="assets/img/roombarat.jpg" alt="">
+                    <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
+                        <h3 class="text-lg lg:text-xl font-medium">RoombaRat (2026)</h3>
+                        <p>CascadiaJS Hackathon 2026, CascadiaJS Seattle</p>
+                    </div>
+                </a>
                 <!-- Project card -->
                 <a href="https://github.com/AccessRetrieved/codegpt" class="block relative mb-16 mt=40">
                     <img class="relative -z-10" src="assets/img/CodeGPT.jpeg" alt="">
@@ -22,6 +33,14 @@ document.addEventListener('DOMContentLoaded', function () {
                     <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
                         <h3 class="text-lg lg:text-xl font-medium">Medlinks (2025)</h3>
                         <p>nwHacks, nwPlus + Major Leagues Hacking</p>
+                    </div>
+                </a>
+                <!-- Project card -->
+                <a href="https://github.com/AccessRetrieved/bdc2024-youth-abuse-ml" class="block relative mb-16">
+                    <img class="relative -z-10" src="assets/img/bdc.png" alt="">
+                    <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
+                        <h3 class="text-lg lg:text-xl font-medium">Big Data Challenge (2024)</h3>
+                        <p>Publication: Identifying Factors Influencing Youth Substance Abuse in Canada: Insights from Machine Learning Models.</p>
                     </div>
                 </a>
                 <!-- Project card -->
@@ -46,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     </div>
                 </a>
                 <!-- Project card -->
-                <a href="https://accessretrieved.github.io/Music/" class="block relative">
+                <a href="https://accessretrieved.github.io/Music/" class="block relative mb-16">
                     <img class="relative -z-10" src="assets/img/oh-my-music.jpg" alt="">
                     <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
                         <h3 class="text-lg lg:text-xl font-medium">Oh My Music (2020)</h3>
@@ -67,12 +86,20 @@ document.addEventListener('DOMContentLoaded', function () {
                 </div>
             </div>
         </div>`;
-    } else {
-        document.getElementById('projects').innerHTML = `
+  } else {
+    document.getElementById("projects").innerHTML = `
         <div class="container grid grid-cols-12 gap-8">
     <div class="col-span-12 md:col-span-6">
         <!-- Project card -->
-        <a href="https://github.com/AccessRetrieved/codegpt" class="block relative mb-16">
+        <a href="https://github.com/AccessRetrieved/RoombaRat" class="block relative mb-16">
+            <img class="relative -z-10" src="assets/img/roombarat.jpg" alt="">
+            <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
+                <h3 class="text-lg lg:text-xl font-medium">RoombaRat (2026)</h3>
+                <p>CascadiaJS Hackathon 2026, CascadiaJS Seattle</p>
+            </div>
+        </a>
+        <!-- Project card -->
+        <a href="https://github.com/AccessRetrieved/codegpt" class="block relative mb-16" style="margin-top: 2.5rem;">
             <img class="relative -z-10" src="assets/img/CodeGPT.jpeg" alt="">
             <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
                 <h3 class="text-lg lg:text-xl font-medium">CodeGPT (2025)</h3>
@@ -81,14 +108,14 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>
         </a>
         <!-- Project card -->
-        <a href="https://constellationhub.org" class="block relative mb-16">
+        <a href="https://constellationhub.org" class="block relative mb-16" style="margin-top: -1.5rem;">
             <img class="relative -z-10" src="assets/img/constellation.jpg" alt="">
             <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
                 <h3 class="text-lg lg:text-xl font-medium">Constellation Networking (2023 - Present)</h3>
             </div>
         </a>
         <!-- Project card -->
-        <a href="" class="block relative mb-16">
+        <a href="" class="block relative" style="margin-top: 3rem;">
             <img class="relative -z-10" src="assets/img/postman.jpg" alt="">
             <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
                 <h3 class="text-lg lg:text-xl font-medium">App Postman (2021)</h3>
@@ -97,7 +124,7 @@ document.addEventListener('DOMContentLoaded', function () {
     </div>
     <div class="col-span-12 md:col-span-6">
         <!-- Project card -->
-        <a href="https://github.com/AccessRetrieved/medlinks" class="block relative mb-16 mt-40">
+        <a href="https://github.com/AccessRetrieved/medlinks" class="block relative mb-16 mt-40 mobile-no-mt">
             <img class="relative -z-10" src="assets/img/medlinks.jpeg" alt="">
             <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
                 <h3 class="text-lg lg:text-xl font-medium">Medlinks (2025)</h3>
@@ -105,14 +132,22 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>
         </a>
         <!-- Project card -->
-        <a href="https://gptnotes.iamjerryhu.org" class="block relative mb-16">
+        <a href="https://github.com/AccessRetrieved/bdc2024-youth-abuse-ml" class="block relative mb-16" style="margin-top: -2rem;">
+            <img class="relative -z-10" src="assets/img/bdc.png" alt="">
+            <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
+                <h3 class="text-lg lg:text-xl font-medium">Big Data Challenge (2024)</h3>
+                <p>Publication: Identifying Factors Influencing Youth Substance Abuse in Canada: Insights from Machine Learning Models.</p>
+            </div>
+        </a>
+        <!-- Project card -->
+        <a href="https://gptnotes.iamjerryhu.org" class="block relative mb-16" style="margin-top: 1.5rem;">
             <img class="relative -z-10" src="assets/img/gptnotes.jpg" alt="">
             <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
                 <h3 class="text-lg lg:text-xl font-medium">GPTNotes (2022 - 2023)</h3>
             </div>
         </a>
         <!-- Project card -->
-        <a href="https://accessretrieved.github.io/Music/" class="block relative mb-16">
+        <a href="https://accessretrieved.github.io/Music/" class="block relative mb-16" style="margin-top: -2.5rem;">
             <img class="relative -z-10" src="assets/img/oh-my-music.jpg" alt="">
             <div class="bg-white w-10/12 pt-4 pr-4 -mt-28">
                 <h3 class="text-lg lg:text-xl font-medium">Oh My Music (2020)</h3>
@@ -132,7 +167,7 @@ document.addEventListener('DOMContentLoaded', function () {
     </div>
 </div>`;
 
-        document.getElementById('cursor-injection').innerHTML += `
+    document.getElementById("cursor-injection").innerHTML += `
         <div id="cursor">
         <div class="cursor-circle circle-big">
             <svg height="30" width="30">
@@ -146,38 +181,38 @@ document.addEventListener('DOMContentLoaded', function () {
         </div>
     </div>`;
 
-        const cursor = document.getElementById("cursor");
-        const cursorBallBig = document.querySelector(".circle-big");
-        const cursorBallSmall = document.querySelector(".circle-small");
+    const cursor = document.getElementById("cursor");
+    const cursorBallBig = document.querySelector(".circle-big");
+    const cursorBallSmall = document.querySelector(".circle-small");
 
-        let posS = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-        let posB = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-        let mouse = { x: posS.x, y: posS.y };
-        const speed = 0.1;
-        let fpms = 60 / 1000;
+    let posS = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+    let posB = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+    let mouse = { x: posS.x, y: posS.y };
+    const speed = 0.1;
+    let fpms = 60 / 1000;
 
-        window.addEventListener("mousemove", (e) => {
-            mouse.x = e.x;
-            mouse.y = e.y;
-        });
+    window.addEventListener("mousemove", (e) => {
+      mouse.x = e.x;
+      mouse.y = e.y;
+    });
 
-        const xSetBallSmall = gsap.quickSetter(cursorBallSmall, "x", "px");
-        const ySetBallSmall = gsap.quickSetter(cursorBallSmall, "y", "px");
-        const xSetBallBig = gsap.quickSetter(cursorBallBig, "x", "px");
-        const ySetBallBig = gsap.quickSetter(cursorBallBig, "y", "px");
+    const xSetBallSmall = gsap.quickSetter(cursorBallSmall, "x", "px");
+    const ySetBallSmall = gsap.quickSetter(cursorBallSmall, "y", "px");
+    const xSetBallBig = gsap.quickSetter(cursorBallBig, "x", "px");
+    const ySetBallBig = gsap.quickSetter(cursorBallBig, "y", "px");
 
-        gsap.ticker.add((time, deltaTime) => {
-            let delta = deltaTime * fpms;
-            let dt = 1.0 - Math.pow(1.0 - speed, delta);
+    gsap.ticker.add((time, deltaTime) => {
+      let delta = deltaTime * fpms;
+      let dt = 1.0 - Math.pow(1.0 - speed, delta);
 
-            posS.x += mouse.x - posS.x;
-            posS.y += mouse.y - posS.y;
-            posB.x += (mouse.x - posB.x) * dt;
-            posB.y += (mouse.y - posB.y) * dt;
-            xSetBallSmall(posS.x);
-            ySetBallSmall(posS.y);
-            xSetBallBig(posB.x);
-            ySetBallBig(posB.y);
-        });
-    }
+      posS.x += mouse.x - posS.x;
+      posS.y += mouse.y - posS.y;
+      posB.x += (mouse.x - posB.x) * dt;
+      posB.y += (mouse.y - posB.y) * dt;
+      xSetBallSmall(posS.x);
+      ySetBallSmall(posS.y);
+      xSetBallBig(posB.x);
+      ySetBallBig(posB.y);
+    });
+  }
 });
